e7dd69ae586d4e7d0cc8ee2b0bacb169f5cb9706
Alex Baker <afastbaker@gmail.com>
2018-04-03 14:28:24 -0700

    This will update naming convention

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function stairsOrDoor() {
 
     if (response === 'TAKE STAIRS') {
       document.getElementById("stairs").classList.toggle('hidden');
-      document.getElementById("intro").classList.toggle('hidden');
+      document.getElementById("story").classList.toggle('hidden');
     } else if (response == 'OPEN DOOR') {
       document.getElementById("door").classList.toggle('hidden');
-      document.getElementById("intro").classList.add('hidden').toggle('hidden');
+      document.getElementById("story").classList.add('hidden').toggle('hidden');
     } else { document.getElementById("error").innerHTML = "You are a Wuss!" }
 }
