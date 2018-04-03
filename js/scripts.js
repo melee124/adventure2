81af88925a16d90265a66c0b3dd8a282d7cf6317
Alex Baker <afastbaker@gmail.com>
2018-04-03 16:34:35 -0700

    This will add age verification console to UL Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//biz logic
 function stairsOrDoor() {
     var response = document.getElementById("response").value;
 
@@ -8,4 +9,20 @@ function stairsOrDoor() {
       document.getElementById("Door").classList.toggle('hidden');
       document.getElementById("story").classList.toggle('hidden');
     } else { document.getElementById("error").innerHTML = "You are a Wuss!" }
+}
+
+//UL Logic
+// Check if the user is ready to play!
+confirm("Ready?")
+
+// Prompt user and check their age.
+var age = prompt("How old are ya?");
+
+if ( age < 13 )
+{
+    console.log("You must be at least 13 to play, but I like you so go ahead.");
+}
+else
+{
+    console.log("YEah, you're old enough... Go ahead!");
 }
