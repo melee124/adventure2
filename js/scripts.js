92e7c54e367e2c9575ecbfca7717cc8fc5b942d6
Alex Baker <afastbaker@gmail.com>
2018-04-03 13:43:38 -0700

    This will add project to github

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function stairsOrDoor() {
+    var response = document.getElementById("response").value;
+
+    if (response === 'TAKE STAIRS') {
+      document.getElementById("stairs").classList.toggle('hidden');
+      document.getElementById("intro").classList.toggle('hidden');
+    } else if (response == 'OPEN DOOR') {
+      document.getElementById("door").classList.toggle('hidden');
+      document.getElementById("intro").classList.add('hidden').toggle('hidden');
+    } else { document.getElementById("error").innerHTML = "Your are a Wuss!" }
+}
